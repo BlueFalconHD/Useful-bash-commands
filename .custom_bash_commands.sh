7ed06f1204eb1405a03bac44743ca4a665b04b2e
BlueFalconHD <72631767+BlueFalconHD@users.noreply.github.com>
2021-05-23 14:18:48 -0500

    updated batt/battery to show battery level in progress bar form
    ++ Progress bar command
    
    progressBar <current percent> <whole percent> <name> <color - $colorname (red etc)>
    
    preview:
    
    Battery [============================================================================================================================================================>...........................................................] ( 73/100)

--- .custom_bash_commands.sh
+++ .custom_bash_commands.sh
@@ -2,6 +2,80 @@ function fapp() {
 	explorer.exe .
 };
 
+trap ctrl_c INT
+
+function ctrl_c() {
+	#Fix colors
+	printf "\n\n\n${textDefault}";
+	exit
+}
+
+textDefault="\e[0m"
+red="\e[31m"
+green="\e[32m"
+yellow="\e[33m"
+blue="\e[34m"
+magenta="\e[35m"
+cyan="\e[36m"
+lightGrey="\e[37m"
+brown="\e[38;5;94m"
+purple="\e[38;5;93m"
+orange="\e[38;5;202m"
+
+function progressBar() {
+	#Input vars
+	local current=${1}
+	local total=${2}
+	local text=${3}
+	local color=${4}
+
+	#Const literal vars
+	local arrowTip=">"
+	local arrowBody="="
+
+	#const calculated  vars
+	local termWidth=$(tput cols)
+	local percent=$(echo "${current} / ${total}" | bc -l)
+
+	local maxDigits=${#total}
+	local constSymbolWidth=$(( (7 + ${#text}) + (${maxDigits} * 2))); #Width of constant symbols in output.
+	local progressBarWidth=$((${termWidth} - ${constSymbolWidth}))
+
+	#Build progress bar
+	local arrow="${arrowTip}"
+
+	#Multiply float, and then convert to int
+	#progressLength=$(printf "%.0f" $(echo "${progressBarWidth} * ${percent}" | bc))
+	progressLength=$(echo "scale=0; (${progressBarWidth} * ${percent})/1" | bc)
+
+	if [ ${current} -eq 0 ]; then
+		arrow=""
+	fi
+
+	for ((i = 1 ; i <= $((${progressLength} - ${#arrowTip})) ; i++)); do
+		if [ ${#arrow} -ge ${progressBarWidth} ]; then
+			break;
+		fi
+		arrow="${arrowBody}${arrow}"
+		if [ ${current} -ge ${total} ]; then
+			current=${total}
+			arrow="${arrow/${arrowTip}/${arrowBody}}"
+		fi
+	done
+	for (( ; i <= ${progressBarWidth} ; i++)); do
+		if [ ${#arrow} -ge ${progressBarWidth} ]; then
+			break;
+		fi
+		arrow="${arrow}."
+	done
+
+
+	printf "${color}${text} [%-${progressBarWidth}s] (%${maxDigits}s/%${maxDigits}s)${textDefault}\r" "${arrow}" "${current}" "${total}"
+}
+
+errors=()
+numErrors=0
+
 function cls() {
 	clear
 };
@@ -23,7 +97,7 @@ function coolcls() {
 	green="\033[4;32m"
 	purple="\033[6;35m"
 	n="\033[0;00m"
-	echo -e "                                                  $blue -------------------$n
+	echo -e "	                                                  $blue -------------------$n
 	$blue                 &@                               $none  $HOSTNAME$n
 	$blue               %&&&              %&&,             $blue -------------------$n
 	$blue               .&&&@         &&@@&&,              $green Bash Version:$n
@@ -84,13 +158,14 @@ function smile() {
         purple="\033[6;35m"
         n="\033[0;00m"
 	echo -e $yellow  $n
-}
+};
 
 function battery() {
 	batterylevel=$(battery-level)
 	battl=${batterylevel::-1}
 	batti=''
 	battc=''
+	ci=''
 
 	RESTORE='\033[0m'
 
@@ -176,7 +251,26 @@ batti=''
 battc=$GREEN
 fi
 
-	echo -e Battery: $battc $batterylevel $batti $RESTORE
+ac_adapter=$(acpi -a | cut -d' ' -f3 | cut -d- -f1)
+
+if [ "$ac_adapter" = "on" ]; then
+    ci=''
+else
+    ci=''
+fi
+
+	echo -e Battery: $battc $batterylevel $ci $batti $RESTORE
+	echo -e $battc $batti Battery Level:
+	progressBar ${battl} 100 'Battery' $battc
+	
+};
+
+function reload() {
+	odir=$(pwd)
+	cd ~
+	source .bashrc
+	source .bash_profile
+	cd $odir
 }
 
 
